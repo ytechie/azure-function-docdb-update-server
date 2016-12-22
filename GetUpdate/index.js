2ed57dd511c75f0d2b738846fc14b253c7b5003b
Jason Young <jason@ytechie.com>
2016-12-22 13:31:24 -0800

    Added a function for getting a log upload location

---
+++ GetUpdate/index.js
@@ -0,0 +1,13 @@
+﻿/*
+
+    A function for downloading an update for a particular node/system.
+
+*/
+
+module.exports = function (context, req) {
+    res = {
+        body: "This function is not yet implemented"
+    };
+
+    context.done(null, res);
+};
---
+++ SendLogs/index.js
@@ -0,0 +1,51 @@
+﻿var azureStorage = require('azure-storage');
+
+//func run .\SendLogs\ -c "{\"nodename\": \"node1\"}"
+
+/*
+
+    Call this function to create a container for the calling system.
+    It returns a blob container URL with a SAS token that allows writing
+    of log files.
+
+*/
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let blobSvc = azureStorage.createBlobService()
+
+    let nodeName = req.query.nodename || req.body.nodename;
+    nodeName = nodeName.toLowerCase();
+
+    blobSvc.createContainerIfNotExists(nodeName, (error, result, response) => {
+        if(!error){
+            context.log('Created container ' + nodeName);
+
+            //The time window we allow nodes to write to the container
+            var startDate = new Date();
+            var expiryDate = new Date(startDate);
+            expiryDate.setMinutes(startDate.getMinutes() + 1000);
+            startDate.setMinutes(startDate.getMinutes() - 100);
+
+            let sharedAccessPolicy = {
+                AccessPolicy: {
+                    Permissions: azureStorage.BlobUtilities.SharedAccessPermissions.WRITE +
+                        azureStorage.BlobUtilities.SharedAccessPermissions.LIST +
+                        azureStorage.BlobUtilities.SharedAccessPermissions.READ,
+                    Start: startDate,
+                    Expiry: expiryDate
+                }
+            }
+
+            var blobSAS = blobSvc.generateSharedAccessSignature(nodeName, null , sharedAccessPolicy);
+            var fullUrl = `https://${process.env['AZURE_STORAGE_ACCOUNT']}.blob.core.windows.net/${nodeName}?${blobSAS}`
+
+            res = {
+                body: fullUrl
+            };
+
+            context.done(null, res);
+        }
+    });
+};
